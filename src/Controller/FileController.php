8522d818645e59c6697de88e1358fc474d1b8c9e
Sullivan <sullivanroirand@protonmail.com>
2018-06-11 22:04:57 +0200

    Files uncategorized added

--- src/Controller/FileController.php
+++ src/Controller/FileController.php
@@ -97,7 +97,7 @@ class FileController extends Controller
     }
 
     /**
-    * @Route("/files/{id}", name="files_category")
+    * @Route("/files/{id}", name="files_category", requirements = { "id" = "[0-9]+" })
     */
     public function categories(Request $request, Category $category)
     {
@@ -206,6 +206,42 @@ class FileController extends Controller
         }
 
         return $this->redirect($this->generateUrl('files'));
+    }
+
+    /**
+    * @Route("/files/uncategorized", name="files_uncategorized")
+    */
+    public function uncategorized(Request $request)
+    {
+        $files = $this->getUser()->getFiles();
+        $files = $files->filter(function($file) {
+            return !$file->getCategory();
+        });
+
+        $user_key_encoded = $this->get('session')->get('encryption_key');
+        $user_key = Key::loadFromAsciiSafeString($user_key_encoded);
+
+        foreach ($files as $file) {
+            $fileName = $file->getFileName();
+            $fileNameLocation = $file->getFileNameLocation();
+
+            try {
+                $fileName = Crypto::decrypt($fileName, $user_key);
+                $fileNameLocation = Crypto::encrypt($fileNameLocation, $user_key);
+            } catch (Defuse\Crypto\Exception\WrongKeyOrModifiedCiphertextException $ex) {
+
+            }
+
+            $file->setFileName($fileName);
+            $file->setFileNameLocation($fileNameLocation);
+        }
+
+        $form = $this->getSearchForm();
+
+        return $this->render('file/index.html.twig', [
+            'files' => $files,
+            'form' => $form->createView()
+        ]);
     }
 
     private function getSearchForm()
